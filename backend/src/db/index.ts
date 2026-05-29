68cd572a01335c216998cdea1e60e90524b6e94c
Md Shahab Uddin <shahabuddin19990108@gmail.com>
2026-05-29 17:59:58 +0600

    Update index.ts

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -4,7 +4,7 @@ import pg from "pg";
 
 import * as schema from "./schema";
 
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL }); 
 
 export const db = drizzle(pool, { schema });
 
