a0ee798c5ba0c9f7847d9b456dedd89d2dae6476
Md Shahab Uddin <shahabuddin19990108@gmail.com>
2026-05-31 03:01:03 +0600

    add

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,8 +16,8 @@ import productRouter from "./routes/productRouter";
 import meRouter from "./routes/meRouter";
 import streamRouter from "./routes/streamRouter";
 import chekoutRouter from "./routes/chekoutRouter";
-// import adminRouter from "./routes/adminRouter";
-// import orderRouter from "./routes/orderRouter";
+import adminRouter from "./routes/adminRouter";
+import orderRouter from "./routes/orderRouter";
 
 import { polarWebhookHandler } from "./webhooks/polar";
 import { sentryClerkUserMiddleware } from "./middleware/sentryClerkUser";
@@ -48,8 +48,8 @@ app.use("/api/me", meRouter);
 app.use("/api/products", productRouter);
 app.use("/api/stream", streamRouter);
 app.use("/api/checkout", chekoutRouter);
-// app.use("/api/admin", adminRouter);
-// app.use("/api/orders", orderRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/orders", orderRouter);
 
 const publicDir = path.join(process.cwd(), "public");
 if (fs.existsSync(publicDir)) {
