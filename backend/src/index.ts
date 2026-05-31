7806aacde59a270a6457cdb8b98f18e53d2dc96f
Md Shahab Uddin <shahabuddin19990108@gmail.com>
2026-05-31 22:45:11 +0600

    done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,7 +27,7 @@ const app = express();
 
 const rawJson = express.raw({ type: "application/json", limit: "1mb" });
 
-// it's important that you don't parse the webhook event data, it should be in the raw format
+// // it's important that you don't parse the webhook event data, it should be in the raw format
 app.post("/webhooks/clerk", rawJson, (req, res) => {
   void clerkWebhookHandler(req, res);
 });
@@ -70,7 +70,7 @@ if (fs.existsSync(publicDir)) {
   });
 }
 
-// sentry will be attached to the response object
+// // sentry will be attached to the response object
 Sentry.setupExpressErrorHandler(app);
 
 app.use(
